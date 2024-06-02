208610d2e64f40f2ed7bcd25ff60a2788c2ea90c
dev-angelist <105108242+dev-angelist@users.noreply.github.com>
2024-06-02 16:53:58 +0200

    Create instructions.php

---
+++ instructions.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en-GB">
+<head>
+    <meta charset="UTF-8">
+    <title>Web Safe Hub :: Vulnerable Web Application</title>
+    <link rel="stylesheet" type="text/css" href="css/main.css" />
+    <link rel="icon" type="image/ico" href="../favicon.ico" />
+ 
+<body>
+<div id="container">
+    <?php
+    include 'menu.php';
+    ?>
+    <div id="main_content">
+        <div id="title_div">
+            <h1>Instructions</h1>
+        </div>
+        <div class="body_padded content_section">
+            
+         <h2>General Instructions</h2>
+   	 <p>WebSafeHub is a PHP/MySQL web application intentionally designed to be vulnerable. It's installable locally for use in secure and controlled environments. To utilize the application, it's necessary to generate a database, which can be done from the <a href="setup_db.php">Setup / Reset DB</a> section. In case of any issues, users can open a ticket on GitHub.</p>
+  	  <h2>How To Contribute</h2>
+  	  WebSafeHub is an open-source project, and contributions are highly encouraged. Users have the capability to create new exercises locally. These exercises can be pushed to GitHub to contribute to the project, helping to expand and enhance the range of training materials available. By sharing your exercises, you not only improve the platform but also aid others in learning and understanding web application security.
+
+To contribute, fork the repository on GitHub, implement your new exercises or improvements, and submit a pull request. Detailed contribution guidelines and documentation are available in the repository to help you get started.
+   	 <hr />
+
+
+        </div>
+    </div>
+</div>
+</body>
+</html>
