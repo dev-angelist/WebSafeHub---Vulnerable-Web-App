97e5607c780d434136ccd67d22a0766303506d10
dev-angelist <105108242+dev-angelist@users.noreply.github.com>
2024-06-02 16:56:17 +0200

    Create brute_force_index.php

---
+++ Vulnerabilities/BruteForce/brute_force_index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en-GB">
+<head>
+    <meta charset="UTF-8">
+    <title>Brute Force :: WebSafeHub</title>
+    <link rel="stylesheet" type="text/css" href="../../css/main.css" />
+    <link rel="icon" type="image/ico" href="../favicon.ico" />
+</head>
+<body>
+<div id="container">
+    <?php
+    $page = 'Brute Force';
+    include '../../menu.php';
+    ?>
+    <div id="main_content">
+        <div id="title_div">
+            <h1>Brute Force</h1>
+        </div>
+        <div class="body_padded content_section">
+            <p>This is the Brute Force page.</p>
+            
+            
+           
+            
+            
+        </div>
+    </div>
+</div>
+</body>
+</html>
